fad47ab91fac018398cf2d7c43d84c3587ef51d5
Malo <malo@sosp.fr>
2022-02-15 15:12:14 +0100

    correction du beug 158

--- pendu/UI/UI.py
+++ pendu/UI/UI.py
@@ -10,9 +10,11 @@ def get_letter():
     letter_is_valid = (len(letter) == 1) and (letter.isalpha())
 
     while not letter_is_valid:  # the entered letter is not valid
-        if len(letter) != 1:
+        if len(letter) > 1:
             letter = input('please, enter only one letter.\n>> ')
-        elif not letter.isalpha():
+        elif len(letter) < 1:
+            letter = input('please, enter one letter.\n>> ')
+        elif letter.isalpha():
+            return letter.upper() # all letters are upped, to compare them easier
+        else:
             letter = input('please, enter a letter\n>> ')
-
-    return letter.upper() # all letters are upped, to compare them easier
