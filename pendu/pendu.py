45633293a050e3ba1d8f46fc9ceede415804eb20
Malo <malo@sosp.fr>
2022-02-16 09:38:15 +0100

    nombre de cops limités. stockage des lettres en mémoire.

--- pendu/pendu.py
+++ pendu/pendu.py
@@ -1,5 +1,5 @@
-from read_files import *
-from UI         import *
+from files_interactions import *
+from UI                 import *
 import random
 
   # goal_is_completed:
@@ -12,12 +12,14 @@ def goal_is_completed(user_word):
 
   # reveal_char:
   # reveal all the [letter] of the word. ex.: all the A
-def reveal_char(user_word, goal_word, letter):
-    letter_is_in_word = False
+def reveal_char(user_word, goal_word, letter, proposed_letters):
+    letter_is_in_word = 0
+    if letter in proposed_letters:
+        return 2
     for i in range(len(goal_word)):
         if goal_word[i] == letter:
             user_word[i] = letter
-            letter_is_in_word = True
+            letter_is_in_word = 1
     return letter_is_in_word
 
   # game:
@@ -28,18 +30,29 @@ def game(dictionary):
     goal_word = dictionary[random.randint(0, len(dictionary) - 1)]
     user_word = ['_'] * len(goal_word)
 
+    proposed_letters = []
+
     nb_try = 0
     while not goal_is_completed(user_word):
+        if len(proposed_letters) > 0:
+            print('You already proposed the letters:', ", ".join(proposed_letters), ".")
+        print('You still can do', 7 - nb_try, 'mistakes.')
         print('This is your word:\n', " ".join(user_word))
+
         letter = UI.get_letter()
-        if reveal_char(user_word, goal_word, letter):
+        return_code = reveal_char(user_word, goal_word, letter, proposed_letters)
+        if return_code == 2:
+            print('You already proposed this letter.\n')
+        elif return_code == 1:
             print('This letter is well in the word.\n')
+            proposed_letters.append(letter)
         else:
             print('No, this letter is not in the word.\n')
+            proposed_letters.append(letter)
             nb_try += 1
 
         if nb_try > 7:
-            print('You did not found the word (', goal_word,') In lower than 7 tryes.\nYou lost!\n')
+            print('You did not found the word (', "".join(goal_word), ') in less than 7 mistakes.\nYou lost!')
             return
 
       # the user won
